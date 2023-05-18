e3d61db481812b487d25b294dbd514bf47fdf4a6
Emlan Implica <111472856+tteoguu@users.noreply.github.com>
2023-05-18 12:01:39 +0800

    Add files via upload

---
+++ Exercise 4/LoginController.java
@@ -0,0 +1,48 @@
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+
+public class LoginController {
+
+    @FXML
+    TextField mytextfield;
+
+    @FXML
+    PasswordField mypasswordfield;
+
+    @FXML
+    Button mybutton;
+
+    @FXML
+    Label mywarningLabel;
+
+    public void login(ActionEvent event) throws IOException{
+
+        String username = mytextfield.getText();
+        String password = mypasswordfield.getText();
+
+        if (username.equals("emlan") && password.equals("123")) {
+
+            Parent root = FXMLLoader.load(getClass().getResource("Scene.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+
+        } else {
+
+            mywarningLabel.setVisible(true);
+            
+        }
+    }
+    
+}
